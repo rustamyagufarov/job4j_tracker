6dd5f3fcc54775e1a8cba5d360e9a5e52c09edca
rustamyagufarov <rustam@yagufarov.ru>
2025-03-30 20:31:11 +0200

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.polymorphism;
+
+public class Bus implements Transport {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
