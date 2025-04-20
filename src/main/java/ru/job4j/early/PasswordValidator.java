353b4d1d7f23e1bd080391cdaced3097a47ee562
rustamyagufarov <rustam@yagufarov.ru>
2025-04-20 12:32:10 +0200

    4. Принципы раннего возврата и охранных выражений [#504875 #540114]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -52,6 +52,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
